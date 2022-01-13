9bffda31d57e3b5e3568282a3974ec4353bd5750
David Villavicencio <villavicencio.david@gmail.com>
2022-01-13 14:39:53 -0600

    Update lspconfig

--- nvim/custom/plugins/lspconfig.lua
+++ nvim/custom/plugins/lspconfig.lua
@@ -2,11 +2,10 @@ require("plugins.configs.lspconfig")
 
 vim.diagnostic.config {
   virtual_text = {
-    prefix = "",
+    prefix = "",
     spacing = 1,
   },
   signs = true,
   underline = true,
   update_in_insert = false,
 }
-
