7d87cfe176e6b0bd3823a467e61a2262566f1a6d
David Villavicencio <villavicencio.david@gmail.com>
2025-04-22 10:57:47 -0700

    Enhance man_colorful function to support default terminal color settings for manpages

--- zsh/functions/man_colorful.sh
+++ zsh/functions/man_colorful.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
 # Colorful manpages
-function man_colorful() {
+# This function is aliased to 'man' for colorful manpages by default.
+man_colorful() {
     env \
-        LESS_TERMCAP_mb="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_md="$(printf "\e[1;31m")" \
-        LESS_TERMCAP_me="$(printf "\e[0m")" \
-        LESS_TERMCAP_se="$(printf "\e[0m")" \
-        LESS_TERMCAP_so="$(printf "\e[1;44;33m")" \
-        LESS_TERMCAP_ue="$(printf "\e[0m")" \
-        LESS_TERMCAP_us="$(printf "\e[1;32m")" \
-            man "$@"
+        LESS_TERMCAP_mb="${LESS_TERMCAP_mb:-$(printf '\033[1;31m')}" \
+        LESS_TERMCAP_md="${LESS_TERMCAP_md:-$(printf '\033[1;31m')}" \
+        LESS_TERMCAP_me="${LESS_TERMCAP_me:-$(printf '\033[0m')}" \
+        LESS_TERMCAP_se="${LESS_TERMCAP_se:-$(printf '\033[0m')}" \
+        LESS_TERMCAP_so="${LESS_TERMCAP_so:-$(printf '\033[1;44;33m')}" \
+        LESS_TERMCAP_ue="${LESS_TERMCAP_ue:-$(printf '\033[0m')}" \
+        LESS_TERMCAP_us="${LESS_TERMCAP_us:-$(printf '\033[1;32m')}" \
+        man "$@"
 }
